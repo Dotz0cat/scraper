930fd759f0e0df1344dbdf5555e33811811c3c60
Dotz0cat <Dotz0cat@gmail.com>
2021-05-31 16:34:15 -0500

    redone things

--- epoch.py
+++ epoch.py
@@ -7,13 +7,14 @@ def loop_page(driver, curser):
 		elem = driver.find_element_by_xpath("/html/body/div[3]/div[1]/div[2]/div[2]/div[1]/ul/li[{0}]/a".format(i))
 		url = elem.get_attribute("href")
 		print(url)
-		data = [url, False]
-		curser.execute("INSERT INTO Urls (Url, Parsed) VALUES (?, ?)", data)
+		date = curser.execute("SELECT strftime('%Y-%m-%d', 'now');")
+		data = [url, date]
+		curser.execute("INSERT INTO Urls (Url, Date_late_scraped) VALUES (?, ?)", data)
 
 con = sqlite3.connect("/home/seth/python/scraper/cache.db")
 c = con.cursor()
 
-c.execute("CREATE TABLE Urls(Url text, Parsed bit)")
+c.execute("CREATE TABLE Urls(Url text, Date_late_scraped text)")
 
 con.commit();
 con.close();
@@ -22,7 +23,7 @@ driver = webdriver.Firefox()
 con = sqlite3.connect("/home/seth/python/scraper/cache.db")
 c = con.cursor()
 
-for i in range(1, 6):
+for i in range(1, 7):
 	driver.get("https://mytuner-radio.com/radio/country/japan-stations?page={0}".format(i))
 	loop_page(driver, c)
 
--- scraper.py
+++ scraper.py
@@ -11,14 +11,11 @@ def save_file(image, directory, name):
 	with open('{dirname}/{name}'.format(dirname=directory, name=name), 'wb') as out_file:
 		shutil.copyfileobj(image.raw, out_file)
 
-def save_image(elem2, name, ext):
+def save_image(dbx, elem2, name, ext):
 	res = requests.get(elem2.get_attribute("src"), stream=True)
 
 	save_file(res, "/home/seth/python/scraper/imgs", name+ext)
 
-	del res
-
-	dbx = dropbox.Dropbox(token)
 	data = open(local_path, "rb").read()
 	path = "/scraper/" + name+ext
 	dbx.files_upload(data, path)
@@ -26,81 +23,57 @@ def save_image(elem2, name, ext):
 
 	return share
 
-def image_exsits(name):
-	dbx = dropbox.Dropbox(token)
+def image_exsits(dbx, name):
 	try:
 		dbx.files_get_metadata("/scraper/" + name)
 		return True
 	except:
 		return False
 
-def get_exsiting(name):
+def get_exsiting(dbx, name):
 	dbx = dropbox.Dropbox(token)
 	return dbx.sharing_get_file_metadata("/scraper/" + name).preview_url
 
-def scrape(address, saved):
+def scrape(dbx, driver, page):
 
-	driver = webdriver.Firefox()
-	driver.get(address)
+	driver.get(page)
 	elem = driver.find_element_by_class_name("title")
 	elem2 = driver.find_element_by_class_name("thumb-img")
 	name = elem.get_attribute("innerHTML").replace(" live", "")
 
 	other, ext = os.path.splitext(elem2.get_attribute("src"))
 	local_path = "/home/seth/python/scraper/imgs/" + name + ext
-	del other
 
-	if image_exsits(name+ext):
-		shared = get_exsiting(name+ext)
+	if image_exsits(dbx, name+ext):
+		shared = get_exsiting(dbx, name+ext)
 	else:
-		shared = save_image(elem2, name, ext)
-
-
-	# res = requests.get(elem2.get_attribute("src"), stream=True)
-	# other, ext = os.path.splitext(elem2.get_attribute("src"))
-	# local_path = "/home/seth/python/scraper/imgs/" + name + ext
-	# del other
-	# save_file(res, "/home/seth/python/scraper/imgs", name+ext)
-# 
-	# del res
-# 
-	# dbx = dropbox.Dropbox(token)
-	# data = open(local_path, "rb").read()
-	# path = "/imgs/imgs/" + name+ext
-	# dbx.files_upload(data, path)
-	# share = dbx.sharing_create_shared_link_with_settings(path)
-
-	#print(dbx.sharing_create_shared_link_with_settings(path).url)
+		shared = save_image(dbx, elem2, name, ext)
 
 	address = driver.execute_script("return _playlist[0].file;")
 
-
-	# fp = open("info.csv", "a+")
-	# fp.write(name)
-	# fp.write(",")
-	# fp.write(share.url.replace("www.dropbox.com", "dl.dropboxusercontent.com").replace("?dl=0", ""))
-	# fp.write(",")
-	# fp.write(driver.execute_script("return _playlist[0].file;"))
-	# fp.write("\n")
-
-	driver.close()
+	#driver.close()
 
 	return name, shared, address
 
 #scrape("https://mytuner-radio.com/radio/soulful-web-station-417283/")
-con = sqlite3.connect("/home/seth/python/scraper/cache.db")
-c = con.cursor()
-for row in c.execute("SELECT Url FROM Urls WHERE Parsed=False"):
-	name, image, address = scrape(row)
-	data = [name, image, address]
-	c.execute("INSERT INTO stations (Name, Image, Address) VALUES (?, ?, ?)", data)
-	time.sleep(10)
-
-con.commit()
-con.close()
-
 
 
 #https://mytuner-radio.com/radio/country/japan-stations
 #/html/body/div[3]/div[1]/div[2]/div[2]/div[1]/ul/li[1]/a
 #/html/body/div[3]/div[1]/div[2]/div[2]/div[1]/ul/li[32]
+
+
+def main():
+	with dropbox.Dropbox(token) as dbx, webdriver.Firefox() as driver:
+		con = sqlite3.connect("/home/seth/python/scraper/cache.db")
+		c = con.cursor()
+
+		for row in c.execute("SELECT Url FROM Urls"):
+			name, image, address = scrape(dbx, driver, row)
+			data = [name, image, address]
+			c.execute("INSERT INTO stations (Name, Image, Address) VALUES (?, ?, ?)", data)
+			c.execute("UPDATE Urls SET Date_last_scraped = strftime('%Y-%m-%d', 'now') WHERE Url = ?", row)
+			time.sleep(10)
+
+		con.commit()
+		con.close()
